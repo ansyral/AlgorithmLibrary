febf9ff379c8514fce2e53c44b07cf5e869eaa47
zhouxuan <lilyzhou0624@hotmail.com>
2017-04-11 00:26:43 +0800

    implement disjoint set forests

---
+++ DataStructure/DisjointSet/DisjointSetForests.cs
@@ -0,0 +1,54 @@
+﻿namespace XuanLibrary.DataStructure.DisjointSet
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class DisjointSetForests<T> : IDisjointSet<T>
+    {
+        private Dictionary<T, T> _parents = new Dictionary<T, T>();
+        private Dictionary<T, int> _ranks = new Dictionary<T, int>();
+
+        public T FindSet(T x)
+        {
+            if (x == null)
+            {
+                throw new ArgumentNullException(nameof(x));
+            }
+            if (!_parents[x].Equals(x))
+            {
+                _parents[x] = FindSet(_parents[x]);
+            }
+            return _parents[x];
+        }
+
+        public void MakeSet(T x)
+        {
+            _parents[x] = x;
+            _ranks[x] = 0;
+        }
+
+        public void Union(T x, T y)
+        {
+            Link(FindSet(x), FindSet(y));
+        }
+
+        private void Link(T x, T y)
+        {
+            int rx = _ranks[x];
+            int ry = _ranks[y];
+            if (rx > ry)
+            {
+                _parents[y] = x;
+            }
+            else
+            {
+                _parents[x] = y;
+                if (rx == ry)
+                {
+                    _ranks[y] = ry + 1;
+                }
+            }
+
+        }
+    }
+}
--- DataStructure/DisjointSet/IDisjointSet.cs
+++ DataStructure/DisjointSet/IDisjointSet.cs
@@ -1,6 +1,11 @@
 ﻿namespace XuanLibrary.DataStructure.DisjointSet
 {
-    public interface IDisjointSet
+    public interface IDisjointSet<T>
     {
+        void MakeSet(T x);
+
+        void Union(T x, T y);
+
+        T FindSet(T x);
     }
 }
