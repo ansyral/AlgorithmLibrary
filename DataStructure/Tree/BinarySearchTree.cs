91fc82c1eefcb73f4c2c2f08c8c40eb1489282ae
zhouxuan <lilyzhou0624@hotmail.com>
2017-04-19 23:36:04 +0800

    add bst methods

--- DataStructure/Tree/BinarySearchTree.cs
+++ DataStructure/Tree/BinarySearchTree.cs
@@ -5,7 +5,7 @@
 
     public class BinarySearchTree<T>
     {
-        public TreeNodeWithParent<T> Root { get; }
+        public TreeNodeWithParent<T> Root { get; private set; }
 
         public IComparer<T> Comparer { get; }
 
@@ -45,34 +45,145 @@
             return null;
         }
 
-        public TreeNodeWithParent<T> Minimum()
+        public TreeNodeWithParent<T> Minimum(TreeNodeWithParent<T> root)
         {
-            throw new NotImplementedException();
+            var cur = root;
+            if (cur == null)
+            {
+                return null;
+            }
+            while (cur.Left != null)
+            {
+                cur = cur.Left;
+            }
+            return cur;
         }
 
-        public TreeNodeWithParent<T> Maximum()
+        public TreeNodeWithParent<T> Maximum(TreeNodeWithParent<T> root)
         {
-            throw new NotImplementedException();
+            var cur = root;
+            if (cur == null)
+            {
+                return null;
+            }
+            while (cur.Right != null)
+            {
+                cur = cur.Right;
+            }
+            return cur;
         }
 
         public TreeNodeWithParent<T> Successor(TreeNodeWithParent<T> node)
         {
-            throw new NotImplementedException();
+            var cur = node;
+            if (cur == null)
+            {
+                return null;
+            }
+            if (cur.Right != null)
+            {
+                return Minimum(cur.Right);
+            }
+            while (cur.Parent != null)
+            {
+                if (cur == cur.Parent.Left)
+                {
+                    return cur.Parent;
+                }
+                cur = cur.Parent;
+            }
+            return null;
         }
 
         public TreeNodeWithParent<T> Predecessor(TreeNodeWithParent<T> node)
         {
-            throw new NotImplementedException();
+            var cur = node;
+            if (cur == null)
+            {
+                return null;
+            }
+            if (cur.Left != null)
+            {
+                return Maximum(cur.Left);
+            }
+            while (cur.Parent != null)
+            {
+                if (cur == cur.Parent.Right)
+                {
+                    return cur.Parent;
+                }
+                cur = cur.Parent;
+            }
+            return null;
         }
 
         public TreeNodeWithParent<T> Insert(TreeNodeWithParent<T> node)
         {
-            throw new NotImplementedException();
+            if (node == null)
+            {
+                throw new ArgumentNullException(nameof(node));
+            }
+            var cur = Root;
+            TreeNodeWithParent<T> parent = null;
+            while (cur != null)
+            {
+                parent = cur;
+                int c = Comparer.Compare(cur.Value, node.Value);
+                if (c > 0)
+                {
+                    cur = cur.Left;
+                }
+                else
+                {
+                    cur = cur.Right;
+                }
+            }
+            node.Parent = parent;
+            if (parent == null)
+            {
+                Root = node;
+            }
+            else if (Comparer.Compare(parent.Value, node.Value) > 0)
+            {
+                parent.Left = node;
+            }
+            else
+            {
+                parent.Right = node;
+            }
+            return parent;
         }
 
         public TreeNodeWithParent<T> Delete(TreeNodeWithParent<T> node)
         {
-            throw new NotImplementedException();
+            var deleted = node;
+            if (deleted == Root)
+            {
+                Root = null;
+                return deleted;
+            }
+            if (deleted.Left != null && deleted.Right != null)
+            {
+                var item = Successor(deleted);
+                T tmp = deleted.Value;
+                deleted.Value = item.Value;
+                item.Value = tmp;
+                deleted = item;
+            }
+            var child = deleted.Left ?? deleted.Right;
+            if (child != null)
+            {
+                child.Parent = deleted.Parent;
+            }
+            if (deleted == deleted.Parent.Left)
+            {
+                deleted.Parent.Left = child;
+            }
+            else
+            {
+                deleted.Parent.Right = child;
+            }
+            return deleted;
         }
 
         private TreeNodeWithParent<T> BuildTree(TreeNodeWithParent<T> cur, int index, T[] nodes)
