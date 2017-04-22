bc222dc44779cc552b42d73aab3d6c79aef5d0f0
zhouxuan <lilyzhou0624@hotmail.com>
2017-04-22 22:33:25 +0800

    add sort algos that take o(n2)

---
+++ Algorithm/SortAlgoSet/OnSquareSort.cs
@@ -0,0 +1,91 @@
+﻿namespace XuanLibrary.Algorithm.SortAlgoSet
+{
+    using System.Collections.Generic;
+
+    public static class OnSquareSort
+    {
+        /// <summary>
+        /// not stable sort(swap elements, hard to keep stable, e.g. 287183564), each iteration gets a minimum
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="comparer"></param>
+        public static void SelectionSort<T>(T[] array, IComparer<T> comparer = null)
+        {
+            comparer = comparer ?? Comparer<T>.Default;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (comparer.Compare(array[j], array[min]) < 0)
+                    {
+                        min = j;
+                    }
+                }
+                if (min != i)
+                {
+                    Swap(array, i, min);
+                }
+            }
+        }
+
+        /// <summary>
+        /// stable sort
+        /// array range[0,i) is sorted
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="comparer"></param>
+        public static void InsertSort<T>(T[] array, IComparer<T> comparer = null)
+        {
+            comparer = comparer ?? Comparer<T>.Default;
+            for (int i = 1; i < array.Length; i++)
+            {
+                T tmp = array[i];
+                int j = i - 1;
+                while (j >= 0 && comparer.Compare(array[j], tmp) > 0)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = tmp;
+            }
+        }
+
+        /// <summary>
+        /// stable sort(only nearby element would swap), each iteration gets a maximum
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="comparer"></param>
+        public static void BubbleSort<T>(T[] array, IComparer<T> comparer = null)
+        {
+            comparer = comparer ?? Comparer<T>.Default;
+            bool sorted;
+            for (int times = 0; times < array.Length; times++)
+            {
+                sorted = true;
+                for (int i = 1; i < array.Length - times; i++)
+                {
+                    if (comparer.Compare(array[i], array[i - 1]) < 0)
+                    {
+                        Swap(array, i, i - 1);
+                        sorted = false;
+                    }
+                }
+                if (sorted)
+                {
+                    break;
+                }
+            }
+        }
+
+        private static void Swap<T>(T[] array, int i, int j)
+        {
+            T temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
---
+++ Test/OnSquareSortAlgoTest.cs
@@ -0,0 +1,144 @@
+﻿namespace XuanLibrary.Test
+{
+    using System.Linq;
+
+    using XuanLibrary.Algorithm.SortAlgoSet;
+    using XuanLibrary.Test.TestDataStructure;
+
+    using Xunit;
+
+    public class OnSquareSortAlgoTest
+    {
+        [Theory]
+        [InlineData(
+            new int[] { 2, 8, 7, 1, 3, 5, 6, 4 },
+            new[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 8, 7, 6, 5, 4, 3, 2, 1 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 8, 7, 6, 5, 1, 2, 3, 4 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        public void TestSelectionSortBasic(int[] source, int[] expected)
+        {
+            OnSquareSort.SelectionSort(source);
+            Assert.Equal(
+                expected,
+                source);
+        }
+
+        [Theory]
+        [InlineData(
+            new int[] { 2, 8, 7, 1, 3, 5, 6, 4 },
+            new[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 8, 7, 6, 5, 4, 3, 2, 1 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 8, 7, 6, 5, 1, 2, 3, 4 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        public void TestInsertSortBasic(int[] source, int[] expected)
+        {
+            OnSquareSort.InsertSort(source);
+            Assert.Equal(
+                expected,
+                source);
+        }
+
+        [Theory]
+        [InlineData(
+            new int[] { 2, 8, 7, 1, 3, 5, 6, 4 },
+            new[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 8, 7, 6, 5, 4, 3, 2, 1 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 8, 7, 6, 5, 1, 2, 3, 4 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        public void TestBubbleSortBasic(int[] source, int[] expected)
+        {
+            OnSquareSort.BubbleSort(source);
+            Assert.Equal(
+                expected,
+                source);
+        }
+
+        [Theory]
+        [InlineData(
+            new string[] { "tank-2", "hello-8", "test-7", "zoo-1", "park-3", "hi-2", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "hi-2", "park-3", "book-4", "tuck-5", "tree-6", "test-7", "hello-8" })]
+        [InlineData(
+            new string[] { "tank-2", "hello-8", "test-7", "zoo-1", "park-3", "hi-4", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "park-3", "hi-4", "book-4", "tuck-5", "tree-6", "test-7", "hello-8" })]
+        [InlineData(
+            new string[] { "tank-2", "hi-4", "hello-8", "test-7", "zoo-1", "park-3", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "park-3", "hi-4", "book-4", "tuck-5", "tree-6", "test-7", "hello-8" })]
+        [InlineData(
+            new string[] { "tank-2", "hello-8", "test-7", "zoo-1", "spec-8", "park-3", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "park-3", "book-4", "tuck-5", "tree-6", "test-7", "spec-8", "hello-8" })]
+        public void TestSelectionSortDuplicate(string[] source, string[] expected)
+        {
+            var s = source.Select(i => (Node)i).ToArray();
+            var e = expected.Select(i => (Node)i).ToArray();
+            OnSquareSort.SelectionSort(s);
+            Assert.Equal(
+                e,
+                s);
+        }
+
+        [Theory]
+        [InlineData(
+            new string[] { "tank-2", "hello-8", "test-7", "zoo-1", "park-3", "hi-2", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "hi-2", "park-3", "book-4", "tuck-5", "tree-6", "test-7", "hello-8" })]
+        [InlineData(
+            new string[] { "tank-2", "hello-8", "test-7", "zoo-1", "park-3", "hi-4", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "park-3", "hi-4", "book-4", "tuck-5", "tree-6", "test-7", "hello-8" })]
+        [InlineData(
+            new string[] { "tank-2", "hi-4", "hello-8", "test-7", "zoo-1", "park-3", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "park-3", "hi-4", "book-4", "tuck-5", "tree-6", "test-7", "hello-8" })]
+        [InlineData(
+            new string[] { "tank-2", "hello-8", "test-7", "zoo-1", "spec-8", "park-3", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "park-3", "book-4", "tuck-5", "tree-6", "test-7", "hello-8", "spec-8" })]
+        public void TestInsertSortDuplicate(string[] source, string[] expected)
+        {
+            var s = source.Select(i => (Node)i).ToArray();
+            var e = expected.Select(i => (Node)i).ToArray();
+            OnSquareSort.InsertSort(s);
+            Assert.Equal(
+                e,
+                s);
+        }
+
+        [Theory]
+        [InlineData(
+            new string[] { "tank-2", "hello-8", "test-7", "zoo-1", "park-3", "hi-2", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "hi-2", "park-3", "book-4", "tuck-5", "tree-6", "test-7", "hello-8" })]
+        [InlineData(
+            new string[] { "tank-2", "hello-8", "test-7", "zoo-1", "park-3", "hi-4", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "park-3", "hi-4", "book-4", "tuck-5", "tree-6", "test-7", "hello-8" })]
+        [InlineData(
+            new string[] { "tank-2", "hi-4", "hello-8", "test-7", "zoo-1", "park-3", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "park-3", "hi-4", "book-4", "tuck-5", "tree-6", "test-7", "hello-8" })]
+        [InlineData(
+            new string[] { "tank-2", "hello-8", "test-7", "zoo-1", "spec-8", "park-3", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "park-3", "book-4", "tuck-5", "tree-6", "test-7", "hello-8", "spec-8" })]
+        public void TestBubbleSortDuplicate(string[] source, string[] expected)
+        {
+            var s = source.Select(i => (Node)i).ToArray();
+            var e = expected.Select(i => (Node)i).ToArray();
+            OnSquareSort.BubbleSort(s);
+            Assert.Equal(
+                e,
+                s);
+        }
+    }
+}
