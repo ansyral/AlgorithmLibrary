e39cee5bdd521f0b36665b4e33825422db984e26
zhouxuan <lilyzhou0624@hotmail.com>
2017-04-22 14:18:17 +0800

    quick sort tests and no, it is not stable!

--- Algorithm/SortAlgoSet/QuickSort.cs
+++ Algorithm/SortAlgoSet/QuickSort.cs
@@ -6,7 +6,7 @@
     public static class QuickSort
     {
         /// <summary>
-        /// stable, use PartitionLastAsPivot.
+        /// notstable, use PartitionLastAsPivot.(87183564)
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="array"></param>
@@ -18,7 +18,7 @@
         }
 
         /// <summary>
-        /// stable, use PartitionFirstAsPivot
+        /// nonstable, use PartitionFirstAsPivot(8743564)
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="array"></param>
@@ -28,6 +28,19 @@
             comparer = comparer ?? Comparer<T>.Default;
             int len = array.Length;
             SortCore(array, 0, len - 1, comparer, PartitionFirstAsPivot);
+        }
+
+        /// <summary>
+        /// not stable, use PartitionTwoWay
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="comparer"></param>
+        public static void SortTwoWay<T>(T[] array, IComparer<T> comparer = null)
+        {
+            comparer = comparer ?? Comparer<T>.Default;
+            int len = array.Length;
+            SortCore(array, 0, len - 1, comparer, PartitionTwoWay);
         }
 
         /// <summary>
@@ -79,11 +92,48 @@
                 }
             }
             Swap(array, i, p);
-            return i + 1;
+            return i;
+        }
+
+        /// <summary>
+        /// use first element as pivot. partition the array range [p,q] range to [p,r-1],[r],[r+1,q]
+        /// [p+1,i) lt or equal to pivot, (j,q] great than or equal to pivot. finally j = i -1
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="array"></param>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <param name="comparer"></param>
+        /// <returns>r</returns>
+        private static int PartitionTwoWay<T>(T[] array, int p, int q, IComparer<T> comparer)
+        {
+            T pivot = array[p];
+            int i = p + 1, j = q;
+            while (i <= j)
+            {
+                while (i <= j && comparer.Compare(array[i], pivot) <= 0)
+                {
+                    i++;
+                }
+                while (i <= j && comparer.Compare(array[j], pivot) >= 0)
+                {
+                    j--;
+                }
+                if (i < j)
+                {
+                    Swap(array, i, j);
+                }
+            }
+            Swap(array, p, j);
+            return j;
         }
 
         private static void SortCore<T>(T[] array, int p, int q, IComparer<T> comparer, Func<T[], int, int, IComparer<T>, int> partitioner)
         {
+            if (p >= q)
+            {
+                return;
+            }
             int r = partitioner(array, p, q, comparer);
             SortCore(array, p, r - 1, comparer, partitioner);
             SortCore(array, r + 1, q, comparer, partitioner);
---
+++ Test/QuickSortAlgoTest.cs
@@ -0,0 +1,167 @@
+﻿namespace XuanLibrary.Test
+{
+    using System;
+    using System.Linq;
+    using XuanLibrary.Algorithm.SortAlgoSet;
+
+    using Xunit;
+
+    public class QuickSortAlgoTest
+    {
+        [Theory]
+        [InlineData(
+            new int[] { 2, 8, 7, 1, 3, 5, 6, 4 },
+            new[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 8, 7, 6, 5, 4, 3, 2, 1 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 8, 7, 6, 5, 1, 2, 3, 4 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        public void TestQuickSortLastAsPivotBasic(int[] source, int[] expected)
+        {
+            QuickSort.SortLastAsPivot(source);
+            Assert.Equal(
+                expected,
+                source);
+        }
+
+        [Theory]
+        [InlineData(
+            new int[] { 2, 8, 7, 1, 3, 5, 6, 4 },
+            new[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 8, 7, 6, 5, 4, 3, 2, 1 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 8, 7, 6, 5, 1, 2, 3, 4 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        public void TestQuickSortFirstAsPivotBasic(int[] source, int[] expected)
+        {
+            QuickSort.SortFirstAsPivot(source);
+            Assert.Equal(
+                expected,
+                source);
+        }
+
+        [Theory]
+        [InlineData(
+            new int[] { 2, 8, 7, 1, 3, 5, 6, 4 },
+            new[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 8, 7, 6, 5, 4, 3, 2, 1 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        [InlineData(
+            new int[] { 8, 7, 6, 5, 1, 2, 3, 4 },
+            new int[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+        public void TestQuickSortTwoWayAsPivotBasic(int[] source, int[] expected)
+        {
+            QuickSort.SortTwoWay(source);
+            Assert.Equal(
+                expected,
+                source);
+        }
+
+        [Theory]
+        [InlineData(
+            new string[] { "tank-2", "hello-8", "test-7", "zoo-1", "park-3", "hi-2", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "hi-2", "park-3", "book-4", "tuck-5", "tree-6", "test-7", "hello-8" })]
+        [InlineData(
+            new string[] { "tank-2", "hello-8", "test-7", "zoo-1", "park-3", "hi-4", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "park-3", "hi-4", "book-4", "tuck-5", "tree-6", "test-7", "hello-8" })]
+        [InlineData(
+            new string[] { "tank-2", "hi-4", "hello-8", "test-7", "zoo-1", "park-3", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "park-3", "hi-4", "book-4", "tuck-5", "tree-6", "test-7", "hello-8" })]
+        [InlineData(
+            new string[] { "tank-2", "hello-8", "test-7", "zoo-1", "spec-8", "park-3", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "park-3", "book-4", "tuck-5", "tree-6", "test-7", "spec-8", "hello-8" })]
+        public void TestQuickSortLastAsPivotDuplicate(string[] source, string[] expected)
+        {
+            // cannot use Enumearble.Cast<T>
+            var s = source.Select(i => (Node)i).ToArray();
+            var e = expected.Select(i => (Node)i).ToArray();
+            QuickSort.SortLastAsPivot(s);
+            Assert.Equal(
+                e,
+                s);
+        }
+
+        [Theory]
+        [InlineData(
+            new string[] { "tank-2", "hello-8", "test-7", "zoo-1", "park-3", "hi-2", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "hi-2", "park-3", "book-4", "tuck-5", "tree-6", "test-7", "hello-8" })]
+        [InlineData(
+            new string[] { "tank-2", "hello-8", "test-7", "zoo-1", "park-3", "hi-4", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "park-3", "book-4", "hi-4", "tuck-5", "tree-6", "test-7", "hello-8" })]
+        [InlineData(
+            new string[] { "tank-2", "hi-4", "hello-8", "test-7", "zoo-1", "park-3", "tuck-5", "tree-6", "book-4" },
+            new string[] { "zoo-1", "tank-2", "park-3", "book-4", "hi-4", "tuck-5", "tree-6", "test-7", "hello-8" })]
+        public void TestQuickSortFirstAsPivotDuplicate(string[] source, string[] expected)
+        {
+            // cannot use Enumearble.Cast<T>
+            var s = source.Select(i => (Node)i).ToArray();
+            var e = expected.Select(i => (Node)i).ToArray();
+            QuickSort.SortFirstAsPivot(s);
+            Assert.Equal(
+                e,
+                s);
+        }
+
+
+        public class Node : IComparable<Node>, IEquatable<Node>
+        {
+            public string Name { get; set; }
+
+            public int Value { get; set; }
+
+            public Node(string name, int val)
+            {
+                Name = name;
+                Value = val;
+            }
+
+            public static explicit operator Node(string str)
+            {
+                string[] parts = str.Split('-');
+                return new Node(parts[0], int.Parse(parts[1]));
+            }
+
+            public int CompareTo(Node other)
+            {
+                return this.Value - other.Value;
+            }
+
+            public bool Equals(Node other)
+            {
+                if (other == null)
+                {
+                    return false;
+                }
+                if (ReferenceEquals(this, other))
+                {
+                    return true;
+                }
+                return Name == other.Name && Value == other.Value;
+            }
+
+            public override bool Equals(object obj)
+            {
+                return Equals(obj as Node);
+            }
+
+            public override int GetHashCode()
+            {
+                return Name.GetHashCode() ^ (Value.GetHashCode() << 1);
+            }
+        }
+    }
+}
