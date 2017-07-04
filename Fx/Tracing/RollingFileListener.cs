a56c81c40d89075447f92d248b8b01e4ed6739a3
zhouxuan <lilyzhou0624@hotmail.com>
2017-07-04 23:23:25 +0800

    add init method

--- Fx/Tracing/RollingFileListener.cs
+++ Fx/Tracing/RollingFileListener.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.Diagnostics;
+    using System.IO;
     using System.Reflection;
 
     public class RollingFileListener : TraceListener
@@ -13,15 +14,27 @@
         private const string ROLLING_SIZEMB = "RollingSizeMB";
         private const string ROLLING_INTERVAL_SECOND = "RollingIntervalS";
         private const string BUFFER_SIZEMB = "BufferSizeMB";
+        private const string FILE_RENTION_SIZEMB = "FileRentionSizeMB";
         private const string FILE_RENTIONTIME_MINUTE = "FileRentionTimeMinute";
         #endregion
 
+        private const string TempRelativePath = "temp";
+        private const string CommitedRelativePath = "commited";
+        private const string Delimeter = "\t";
+        private readonly string[] _columns = new[] { "DeploymentId", "Role", "RoleInstance", "Timestamp", "EventTickCount", "EventId", "Level", "Pid", "Tid", "Message" };
         private readonly Dictionary<string, object> _environmentVariables = new Dictionary<string, object>();
         private string _tracePath;
-        private int? _rollingSizeMB;
-        private int? _rollingIntervalSec;
-        private int _bufferSizeMB;
-        private int _fileRentionMinute;
+        private TextWriter _writer;
+        private int _rollingSizeMB = 128;
+        private int _rollingIntervalSec = 60;
+        private int _bufferSizeMB = 5 * 1024;
+        private int _fileRentionSizeMB = 1024;
+        private int _fileRentionMinute = 20;
+
+        private string Header
+        {
+            get { return string.Join(Delimeter, _columns); }
+        }
 
         public RollingFileListener()
         {
@@ -59,11 +72,19 @@
             }
             if (Attributes.ContainsKey(ROLLING_INTERVAL_SECOND))
             {
-                _rollingSizeMB = Convert.ToInt32(Attributes[ROLLING_INTERVAL_SECOND]);
+                _rollingIntervalSec = Convert.ToInt32(Attributes[ROLLING_INTERVAL_SECOND]);
             }
-            if (Attributes.ContainsKey(ROLLING_SIZEMB))
+            if (Attributes.ContainsKey(BUFFER_SIZEMB))
             {
-                _rollingSizeMB = Convert.ToInt32(Attributes[ROLLING_SIZEMB]);
+                _bufferSizeMB = Convert.ToInt32(Attributes[BUFFER_SIZEMB]);
+            }
+            if (Attributes.ContainsKey(FILE_RENTION_SIZEMB))
+            {
+                _fileRentionSizeMB = Convert.ToInt32(Attributes[FILE_RENTION_SIZEMB]);
+            }
+            if (Attributes.ContainsKey(FILE_RENTIONTIME_MINUTE))
+            {
+                _fileRentionMinute = Convert.ToInt32(Attributes[FILE_RENTIONTIME_MINUTE]);
             }
         }
     }
